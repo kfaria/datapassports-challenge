0db46a41c9011a9b74a9e684dceba4577d0a90dd
Ken Faria <kennethfaria@gmail.com>
2020-12-13 14:32:48 -0500

    Card starting point

---
+++ src/components/XMLForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card'
+
+function XMLForm() {
+  
+  return (
+    <Card style={{ width: '20rem' }}>
+      <Card.Body>
+        <Card.Title>Technical Challenge</Card.Title>
+      </Card.Body>
+    </Card>
+  )
+}
+
+export default XMLForm
